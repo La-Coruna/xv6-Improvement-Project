c5a7b2332306d7df1a4e4becdfb607fc0b1a5a73
la-coruna <jyp0430@naver.com>
2023-05-16 04:40:10 +0000

    create exec2()

--- project2/xv6-public/defs.h
+++ project2/xv6-public/defs.h
@@ -25,6 +25,9 @@ void            panic(char*) __attribute__((noreturn));
 // exec.c
 int             exec(char*, char**);
 
+// exec2.c
+int             exec2(char*, char**);
+
 // file.c
 struct file*    filealloc(void);
 void            fileclose(struct file*);
