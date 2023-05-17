4dfd53be2823419fbfaaeef593f1243000986ac0
Park Jongyun <2019019043@hanyang.ac.kr>
2023-05-17 07:05:21 +0000

    pmanager.c: implement parsing command

--- project2/xv6-public/pmanager.c
+++ project2/xv6-public/pmanager.c
@@ -13,6 +13,8 @@
 
 #define MAXARGLENGTH 10
 
+#define WHITESPACE " \t\r\n\v"
+
 struct pmgrcmd {
   int type;
 };
@@ -33,7 +35,8 @@ struct cmdarg2 {
 };
 
 // function declaration
-int strncmp(const char *p, const char *q, uint n);
+void skipWhitespace(char ** ss, char * es);
+char* getWord(char ** ss, char * es);
 int getPmgrCmd(char *buf, int nbuf);
 struct pmgrcmd* parsecmd(char *s);
 void runPmgrCmd(struct pmgrcmd *cmd);
@@ -52,16 +55,37 @@ int main(int argc, char *argv[])
   exit();
 }
 
-//function definition
+// function definition
 
-int
-strncmp(const char *p, const char *q, uint n)
+// skip the whitespace, so move to the front of the next word
+void
+skipWhitespace(char ** s, char * es)
 {
-  while(n > 0 && *p && *p == *q)
-    n--, p++, q++;
-  if(n == 0)
-    return 0;
-  return (uchar)*p - (uchar)*q;
+  while(*s < es && strchr(WHITESPACE, **s)){
+    (*s)++;
+  }
+  //printf("in function s : %d\n", *s);
+  return;
+}
+
+// Get the front word from string. if there is some whitespace, ignore it.
+char *
+getWord(char ** s, char * es)
+{
+  // skip whitespace
+  skipWhitespace(s,es);
+
+  // get a word (till reach whitespace)
+  char *arg = malloc(sizeof(char)*MAXARGLENGTH);
+  memset(arg, 0, sizeof(char)*MAXARGLENGTH);
+  char * ss = *s;
+  while(*s < es && !strchr(WHITESPACE, **s)){
+      (*s)++;
+  }
+  **s = 0; // 문자열 끝에 널문자 추가
+  (*s)++;
+  strcpy(arg, ss);
+  return arg;
 }
 
 struct pmgrcmd*
@@ -123,116 +147,55 @@ parsecmd(char *s)
   // nulterminate(cmd);
   // return cmd;
 
-  char *ss; // end of string
+  //char *ss; // end of string
   char *es; // end of string
-  int cmdlen = 0; // length of command
+  char *commandName;
   struct pmgrcmd *pmgrcmd = 0;
 
-  //ss = s;
   es = s + strlen(s);
   printf(1, "input is %s\n",s);
-  printf(1, "zinput is %d\n",strncmp(s,"list",cmdlen));
+  printf(1, "zinput is %d\n",strcmp(s,"list"));
 
-  // //TODO 공백을 기준으로 parsing 해야함
-  // if (strcmp(s,"list")==0){
-  //   pmgrcmd = alloc_cmdarg0();
-  //   pmgrcmd->type=LIST;
-  // }
-  // else if (strcmp(s,"kill")==0){
-  //   pmgrcmd = alloc_cmdarg0();
-  //   pmgrcmd->type=KILL;
-  // }
-  // else if (strcmp(s,"exit")==0){
-  //   pmgrcmd = alloc_cmdarg0();
-  //   pmgrcmd->type=EXIT;
-  // }
-
-//TODO 이 while문 이용하면 될듯
-  // printf(1,"s: %s\n", s);
-  // printf(1,"s: %d, es: %d\n",s,es);
-  // printf(1,"s: %d, es: %d\n",s,es);
-  
-  // 명령어 앞에 공백 지우기
-  while(s < es && strchr(whitespace, *s)){
-    s++;
-  }
-
-  // 다음 공백까지 끊기 (명령어가 무엇인지 확인하기 위해)
-  ss = s;
-  printf(1,"s: %s",s); // ! for debug
-  //TODO 이 while문 모듈화가 가능할 것 같은데... 빈칸 없애는 함수1 그리고 다음 빈칸까지 strcpy해서 새로운 문자열로 반환하는 거 1
-  while(s < es && !strchr(whitespace, *s)){
-    s++;
-    cmdlen++;
-  }
-  printf(1,"s: %s",s); // ! for debug
-  s++;
-  printf(1,"s: %s",s); // ! for debug
   // 어떤 명령어인지 확인
-  if(cmdlen == 4){
-    // list
-    if (strncmp(ss,"list",cmdlen)==0){
-      pmgrcmd = alloc_cmdarg0();
-      pmgrcmd->type=LIST;
-    }
-
-    // kill
-    else if (strncmp(ss,"kill",cmdlen)==0){
-      // parsing into argument
-      char *arg = malloc(sizeof(char)*MAXARGLENGTH);
-      memset(arg, 0, sizeof(char)*MAXARGLENGTH);
-      //printf(1,"s: %s\n",s );// ! for debug
-      ss = s;
-      //printf(1,"ss: %s\n",ss );// ! for debug
-      //printf(1,"ss pointer: %d\n",ss );// ! for debug
-      while(s < es && !strchr(whitespace, *s)){
-        //printf(1,"+ss pointer: %d\n",ss );// ! for debug
-        s++;
-      }
-      //printf(1,"rss pointer: %d\n",ss );// ! for debug
-      *s = 0;
-      //printf(1,"ss: %s\n",ss );// ! for debug
-      strcpy(arg, ss);
-      //printf(1,"arg: %s\n",arg );// ! for debug
-      pmgrcmd = alloc_cmdarg1(arg);
-      pmgrcmd->type=KILL;
-    }
-
-    // exit
-    else if (strncmp(ss,"exit",cmdlen)==0){
-      pmgrcmd = alloc_cmdarg0();
-      pmgrcmd->type=EXIT;
-    }
+  commandName = getWord(&s,es);
+  // list
+  if (strcmp(commandName,"list")==0){
+    pmgrcmd = alloc_cmdarg0();
+    pmgrcmd->type=LIST;
   }
-  else if (cmdlen==7 && strncmp(ss,"execute",cmdlen)==0){
+
+  // kill
+  else if (strcmp(commandName,"kill")==0){
+    // parsing into argument
+    char *arg = getWord(&s,es);
+    pmgrcmd = alloc_cmdarg1(arg);
+    pmgrcmd->type=KILL;
+  }
+
+  // execute
+  else if (strcmp(commandName,"execute")==0){
       // parsing into argument
-      char *arg1 = malloc(sizeof(char)*MAXARGLENGTH);
-      char *arg2 = malloc(sizeof(char)*MAXARGLENGTH);
-      memset(arg1, 0, sizeof(char)*MAXARGLENGTH);
-      memset(arg2, 0, sizeof(char)*MAXARGLENGTH);
-      //printf(1,"s: %s\n",s );// ! for debug
-      ss = s;
-      //printf(1,"ss: %s\n",ss );// ! for debug
-      //printf(1,"ss pointer: %d\n",ss );// ! for debug
-      while(s < es && !strchr(whitespace, *s)){
-        //printf(1,"+ss pointer: %d\n",ss );// ! for debug
-        s++;
-      }
-      //printf(1,"rss pointer: %d\n",ss );// ! for debug
-      *s = 0;
-      strcpy(arg1, ss);
-      ss = ++s;
-      while(s < es && !strchr(whitespace, *s)){
-        //printf(1,"+ss pointer: %d\n",ss );// ! for debug
-        s++;
-      }
-      *s = 0;
-      //printf(1,"ss: %s\n",ss );// ! for debug
-      strcpy(arg2, ss);
-      //printf(1,"arg: %s\n",arg );// ! for debug
+      char *arg1 = getWord(&s,es);
+      char *arg2 = getWord(&s,es);
       pmgrcmd = alloc_cmdarg2(arg1,arg2);
       pmgrcmd->type=EXEC;
     ;
+  }
+
+  // memlim
+  else if (strcmp(commandName,"memlim")==0){
+      // parsing into argument
+      char *arg1 = getWord(&s,es);
+      char *arg2 = getWord(&s,es);
+      pmgrcmd = alloc_cmdarg2(arg1,arg2);
+      pmgrcmd->type=MEMLIM;
+    ;
+  }
+
+  // exit
+  else if (strcmp(commandName,"exit")==0){
+    pmgrcmd = alloc_cmdarg0();
+    pmgrcmd->type=EXIT;
   }
   printf(1,"pmgrcmd=%d\n",pmgrcmd);
   return pmgrcmd;
@@ -242,13 +205,16 @@ parsecmd(char *s)
 void
 runPmgrCmd(struct pmgrcmd *pmgrcmd)
 {
-  // struct listcmd *lcmd;
-  struct cmdarg1 *kcmd;
-  struct cmdarg2 *ecmd;
+  struct cmdarg0 *cmd0;
+  struct cmdarg1 *cmd1;
+  struct cmdarg2 *cmd2;
   // struct memlimcmd *mlcmd;
 
-  if(pmgrcmd == 0)
-    exit();
+  // 명세 외의 command를 입력했을 때 예외처리
+  if(pmgrcmd == 0){
+    printf(1,"wrong command.\n");
+    return;
+  }
   //printf(1, "pmgrcmd->type: %d\n",pmgrcmd->type); // ! for debug
   switch(pmgrcmd->type){
   default:
@@ -257,26 +223,23 @@ runPmgrCmd(struct pmgrcmd *pmgrcmd)
 
   case LIST:
     printf(1,"list\n");
-    // lcmd = (struct listcmd*)cmd;
-    // if(fork1() == 0)
-    //   runcmd(lcmd->left);
-    // wait();
-    // runcmd(lcmd->right);
+    cmd0 = (struct cmdarg0 *) pmgrcmd;
+    printf(1,"list type: %d\n",cmd0->type);
     break;
 
   case KILL:
-    kcmd = (struct cmdarg1 *) pmgrcmd;
-    printf(1,"kill type: %d\n",kcmd->type);
-    printf(1,"kill pid: %d\n",kcmd->arg);
-    printf(1,"kill pid: %s\n",kcmd->arg);
+    cmd1 = (struct cmdarg1 *) pmgrcmd;
+    printf(1,"kill type: %d\n",cmd1->type);
+    printf(1,"kill pid: %d\n",cmd1->arg);
+    printf(1,"kill pid: %s\n",cmd1->arg);
     break;
 
   case EXEC:
+    cmd2 = (struct cmdarg2 *) pmgrcmd;
     printf(1,"exec\n");
-    ecmd = (struct cmdarg2 *) pmgrcmd;
-    printf(1,"exec type: %d\n",ecmd->type);
-    printf(1,"exec pid: %s\n",ecmd->arg1);
-    printf(1,"exec pid: %s\n",ecmd->arg2);
+    printf(1,"exec type: %d\n",cmd2->type);
+    printf(1,"exec pid: %s\n",cmd2->arg1);
+    printf(1,"exec pid: %s\n",cmd2->arg2);
     // ecmd = (struct execcmd*)cmd;
     // if(ecmd->argv[0] == 0)
     //   exit();
