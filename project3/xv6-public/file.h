3ba21242eeb29e82c0edc3e83d7eb74c9cdac55b
la-coruna <jyp0430@naver.com>
2023-06-06 13:36:26 +0000

    symbolic link: inode 구조 추가

--- project3/xv6-public/file.h
+++ project3/xv6-public/file.h
@@ -23,6 +23,7 @@ struct inode {
   short nlink;
   uint size;
   uint addrs[NDIRECT+3]; // direct와 single,double,triple indirect
+  struct inode * origin_inode;
 };
 
 // table mapping major device number to
